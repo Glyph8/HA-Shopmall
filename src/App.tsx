f288da3092c1606fcae6e3e8ee1e804f6ecd5e62
Ga Min Cha <prettygamin@naver.com>
2026-01-14 16:34:40 +0900

    feat:product-list

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Layout from "./components/Layout/Layout";
+import ProductList from "./pages/ProductList/ProductList";
 
-function App() {}
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="products" element={<ProductList />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
